50f8f5500ef00d00c4cf38ad7e6dbd10b1bd28a0
Felihenrikez <fehenriquez@gmail.com>
2024-06-13 12:44:39 -0400

    circulo al 90

--- src/app/componentes/formulario-circulo/formulario-circulo.component.ts
+++ src/app/componentes/formulario-circulo/formulario-circulo.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit, input } from '@angular/core';
-import { IonItem,IonInput } from '@ionic/angular/standalone';
+import { Component, OnInit,Input } from '@angular/core';
+import { IonItem,IonInput,IonLabel,IonImg,IonButton } from '@ionic/angular/standalone';
+import { circulo } from 'src/app/modelo/clases';
+
 
 @Component({
   selector: 'app-formulario-circulo',
   templateUrl: './formulario-circulo.component.html',
   styleUrls: ['./formulario-circulo.component.scss'],
   standalone: true,
-  imports:[IonInput,IonItem]
+  imports:[IonItem,IonInput,IonLabel,IonImg,IonButton]
 })
 export class FormularioCirculoComponent  implements OnInit {
+  txtradio=""
+  perimetro=0
+  circulo =new circulo("circulo",0)
+  txtMensaje=" "
 
   constructor() { }
+CalcularPerimetro(){
+  this.circulo.radio=parseInt(this.txtradio)
+  this.perimetro=this.circulo.calcularPerimetro()
+}
 
   ngOnInit() {}
 
